070fc1ce660fa3af81cea458451a764fde64c75d
Grzegorz Opara <grzegorz.opara@gmail.com>
2024-02-23 09:13:08 +0000

    minor fixes

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ### GCP
 - GCS Bucket to store tf state file: 
 ``
-gcloud storage buckets create gs://gopara-gke-lab-gcs-tf
+gcloud storage buckets create gs://gopara-gke-lab-gcs-tf --project gopara-gke-lab --location europe-west4 --uniform-bucket-level-access
 ``
 
 ### Other
--- main.tf
+++ main.tf
@@ -1,20 +1,19 @@
 provider "google" {
-  project = "${var.project_id}"
-  region = "${var.region}"
+  project = var.project_id
+  region = var.region
 }
 
 module "network" {
-  source = "../../modules/network"
+  source = "./modules/network"
 
-  project_id          = "${var.project_id}"
-  region              = "${var.region}"
+  project_id          = var.project_id
+  region              = var.region
 }
 
 module "gke" {
-  source = "../../modules/gke"
+  source = "./modules/gke"
 
-  version_prefix = "${var.version_prefix}"
-  project_id          = "${var.project_id}"
-  region              = "${var.region}"
+  project_id          = var.project_id
+  region              = var.region
 
 }
