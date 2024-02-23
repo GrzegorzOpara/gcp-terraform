c815fe0855daa3ab88a43345a6a8935c5d7f03f1
Grzegorz Opara <grzegorz.opara@gmail.com>
2024-02-23 07:33:25 +0000

    GKE tf deployment - version 0.1

--- main.tf
+++ main.tf
@@ -1,13 +1,62 @@
 provider "google" {
   project = "${var.project_id}"
+  region = "${var.region}"
 }
 
-resource "google_storage_bucket" "dev-gcs" {
-  name          = "${var.project_id}-gcs-bucket"
-  location      = "${var.location}"
-  
-  storage_class = "STANDARD"
+# vpc
+resource "google_compute_network" "vpc" {
+  name                    = "${var.project_id}-vpc"
+  auto_create_subnetworks = "false"
+}
 
-  force_destroy = true
-  uniform_bucket_level_access = true
+# subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.project_id}-gke-subnet"
+  region        = var.region
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.10.0.0/24"
+}
+
+# gke cluster
+data "google_container_engine_versions" "gke_version" {
+  location = var.region
+  version_prefix = "1.27."
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "${var.project_id}-gke"
+  location = var.zone
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+}
+
+# Separately Managed Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = google_container_cluster.primary.name
+  location   = var.zone
+  cluster    = google_container_cluster.primary.name
+  
+  version = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
+  node_count = var.gke_num_nodes
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    preemptible  = true
+    machine_type = "n1-standard-1"
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,2 +1,23 @@
-variable "project_id" {}
-variable "location" {}
+variable "project_id" {
+    description = "deployment project"  
+}
+
+variable "region" {
+  description = "deployment region"  
+}
+
+variable "zone" {
+  description = "deployment zone"  
+}
+
+variable "gke_username" {
+  description = "gke username"
+}
+
+variable "gke_password" {
+  description = "gke password"
+}
+
+variable "gke_num_nodes" {
+  description = "number of gke nodes"
+}
