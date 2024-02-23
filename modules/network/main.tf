f01337692accaf4a442b996d021e30d8408b3c39
Grzegorz Opara <grzegorz.opara@gmail.com>
2024-02-23 14:49:12 +0000

    enabled private_ip_google_access

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -2,6 +2,7 @@
 resource "google_compute_network" "vpc" {
   name                    = "${var.project_id}-vpc"
   auto_create_subnetworks = "false"
+  private_ip_google_access = "true"
 }
 
 # GKE subnet
